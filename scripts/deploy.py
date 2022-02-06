fe8b81e04fcac3fc6cd5cb6cb92a27216eedd118
Selçuk <selcukxxx12@gmail.com>
2022-02-06 20:44:13 +0300

    Added publish source for testnet

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,4 @@
-import json
-
-from brownie import BallotBox, VoterToken
+from brownie import BallotBox, VoterToken, config, network
 
 from scripts.helpful_scripts import get_account, get_mock_accounts
 
@@ -10,7 +8,11 @@ def deploy_ballot_box():
     account = get_account()
     voter_token = VoterToken.deploy(len(voters), {"from": account})
     ballot_box = BallotBox.deploy(
-        voter_token.address, voters, candidates, {"from": account}
+        voter_token.address,
+        voters,
+        candidates,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
     )
 
     tx = voter_token.transfer(
